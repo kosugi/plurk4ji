07550783160a816b787273db8638ebb7481c854f
kosugi <kosugi@BRYANSK.local>
2014-01-09 18:20:28 +0900

    divided long posting text

--- cmd_balance.py
+++ cmd_balance.py
@@ -10,6 +10,30 @@ def do_post(balancer, content):
 def is_god_reading_fortune(plurk):
     return 'asks' == plurk.get('qualifier') and 0 <= plurk.get('content_raw', '').find(u'神')
 
+def infer_split_position(text, threshold):
+    soft_threshold = int(threshold * 0.8)
+    if len(text) % soft_threshold < threshold * 0.19:
+        soft_threshold = threshold
+    return soft_threshold
+
+def split_text(text, threshold=100):
+    t = infer_split_position(text, threshold)
+    l = len(text)
+    p = 0
+    texts = []
+    while p < l:
+        texts.append(text[p:p+t])
+        p += t
+    return texts
+
+def uniquify(texts):
+    newset = []
+    for text in texts:
+        while text in newset:
+            text += u'\u200b' # TODO: might reach maxlen (140?)
+        newset.append(text)
+    return newset
+
 def do(config):
 
     api = Api(config['api'])
@@ -41,13 +65,14 @@ def do(config):
 
             complement = balancer.complement
             if 0 < len(complement):
-                logging.info('  posting: %s' % complement)
-                params = dict(plurk_id=plurk_id, qualifier=':', content=complement.encode('UTF-8'))
-                ok, status, data = api.post('/Responses/responseAdd', params)
-                if not ok:
-                    logging.error('''api.post('/Responses/responseAdd', dict(content=%s))''' % complement)
-                    logging.error('status: %s' % status)
-                    logging.error(data)
+                for text in uniquify(split_text(complement)):
+                    logging.info('  posting: %s' % text)
+                    params = dict(plurk_id=plurk_id, qualifier=':', content=text.encode('UTF-8'))
+                    ok, status, data = api.post('/Responses/responseAdd', params)
+                    if not ok:
+                        logging.error('''api.post('/Responses/responseAdd', dict(content=%s))''' % complement)
+                        logging.error('status: %s' % status)
+                        logging.error(data)
                     return
 
 if __name__ == '__main__':
--- test_cmd_balance.py
+++ test_cmd_balance.py
@@ -17,3 +17,24 @@ class TestCmdBalance(unittest.TestCase):
         self.assertTrue (cmd_balance.is_god_reading_fortune(dict(qualifier=u'asks', content_raw=u'---神 一富士二鷹三茄子---')), u'実例')
         self.assertTrue (cmd_balance.is_god_reading_fortune(dict(qualifier=u'asks', content_raw=u'神 もう一度')), u'実例')
         self.assertTrue (cmd_balance.is_god_reading_fortune(dict(qualifier=u'asks', content_raw=u'神　もう一度')), u'実例')
+
+    def test_split_text(self):
+        self.assertEqual(cmd_balance.split_text('', 9), [])
+        self.assertEqual(cmd_balance.split_text('1', 9), ['1'])
+        self.assertEqual(cmd_balance.split_text('12345678', 9),        ['12345678'])
+        self.assertEqual(cmd_balance.split_text('123456789', 9),       ['1234567', '89'])
+        self.assertEqual(cmd_balance.split_text('1234567890', 9),      ['1234567', '890'])
+        self.assertEqual(cmd_balance.split_text('1234567890', 9),      ['1234567', '890'])
+        self.assertEqual(cmd_balance.split_text('12345678901', 9),     ['1234567', '8901'])
+        self.assertEqual(cmd_balance.split_text('123456789012', 9),    ['1234567', '89012'])
+        self.assertEqual(cmd_balance.split_text('1234567890123', 9),   ['1234567', '890123'])
+        self.assertEqual(cmd_balance.split_text('12345678901234', 9),  ['123456789', '01234'])
+        self.assertEqual(cmd_balance.split_text('123456789012345', 9), ['123456789', '012345'])
+        self.assertEqual(cmd_balance.split_text('1234567890123456', 9), ['1234567', '8901234', '56'])
+
+    def test_uniquify(self):
+        self.assertEqual(cmd_balance.uniquify([]), [])
+        self.assertEqual(cmd_balance.uniquify(['a']), ['a'])
+        self.assertEqual(cmd_balance.uniquify(['a', 'b']), ['a', 'b'])
+        self.assertEqual(cmd_balance.uniquify(['a', 'a']), ['a', u'a\u200b'])
+        self.assertEqual(cmd_balance.uniquify(['a', 'a', 'a']), [u'a', u'a\u200b', u'a\u200b\u200b'])
